3eaaa4118cfa8b43d2870aac7d3711da5fd65ce3
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2016-09-25 12:37:25 -0500

    Added Changes

--- src/Main/TCPMain.java
+++ src/Main/TCPMain.java
@@ -1,8 +1,11 @@
 package Main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,7 +14,7 @@ public class TCPMain {
 	public static void main(String args[]) throws Exception
     {
 	
-
+	System.out.println("Starting TCP connection on port: 6789");
     String clientSentence;
     String capitalizedSentence;
     ServerSocket welcomeSocket = new ServerSocket(6789);
@@ -22,10 +25,32 @@ public class TCPMain {
        BufferedReader inFromClient =
        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+       PrintWriter out = new PrintWriter(new BufferedWriter(
+               new OutputStreamWriter(connectionSocket.getOutputStream())),
+               true);
        clientSentence = inFromClient.readLine();
-       System.out.println("Received: " + clientSentence);
-//       capitalizedSentence = clientSentence.toUpperCase() + '\n';
-//       outToClient.writeBytes(capitalizedSentence);
+       if (clientSentence.equalsIgnoreCase("Hello")) {
+    	   out.println("HelloBack");
+    	   
+    	   String readed = "";
+    	   readed = inFromClient.readLine();
+    	   while (readed.equalsIgnoreCase("Bye") == false) {
+    		   
+    		   System.out.println("Client: " + connectionSocket.getRemoteSocketAddress() + " - Sent Location: " + readed);
+    		   
+    		   readed = inFromClient.readLine();
+    	   }
+    	   
+    	   if (readed.equalsIgnoreCase("Bye")) {
+    		   connectionSocket.close();
+    	   }
+    	   
+    	   
+       } else {
+    	   out.println("Close");
+    	   connectionSocket.close();
+       }
+
     }
     
     }
--- src/Main/UDPMain.java
+++ src/Main/UDPMain.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
@@ -8,6 +9,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class UDPMain {
 	
@@ -15,25 +17,20 @@ public class UDPMain {
 	
 	public static void main(String args[]) throws Exception
     {
-		
+		System.out.println("Starting UDP connection on port: 9876");
 		
        DatagramSocket serverSocket = new DatagramSocket(9876);
           byte[] receiveData = new byte[1024];
-          //byte[] sendData = new byte[1024];
           while(true)
              {
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 serverSocket.receive(receivePacket);
                 String sentence = new String( receivePacket.getData());
-                System.out.println("RECEIVED: " + sentence);
                 InetAddress IPAddress = receivePacket.getAddress();
                 int port = receivePacket.getPort();
-                System.out.println("The IPAddress is: " + IPAddress +" And the User Port is: " + port);
-//                String capitalizedSentence = sentence.toUpperCase();
-//                sendData = capitalizedSentence.getBytes();
-//                DatagramPacket sendPacket =
-//                new DatagramPacket(sendData, sendData.length, IPAddress, port);
-//                serverSocket.send(sendPacket);
+                
+                System.out.println("Client IPAddress: " + IPAddress +", Port: " + port + " - Sent Location: " + sentence);
+                
              }
           
           
